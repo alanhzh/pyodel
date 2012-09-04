0dfe46637d432f503d0112578d6a3d8a7658e128
Alan Etkin <spametki@gmail.com>
2012-09-04 07:36:59 -0300

    Test app files

---
+++ controllers/default.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a samples controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+## - call exposes all registered services (none by default)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simple replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Welcome to web2py!")
+    return dict(message=T('Hello World'))
+    
+def edit():
+    form = SQLFORM(db.wiki_page)
+    return dict(form=form)
+    
+def wiki():
+    return dict(wiki=auth.wiki(), rows=db(db.wiki_page).select())
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request,db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
+@auth.requires_signature()
+def data():
+    """
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs must be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
+
+@auth.requires_login()
+def test():
+    gradebook=LOAD(c="plugin_pyodel", f="gradebook.load",
+                   args=["gradebook", 1, "mode", "edit"],
+                   ajax=True)
+    admission=LOAD(c="plugin_pyodel", f="admission.load",
+                   args=["course", 1], ajax=True)
+    sandglass=LOAD(c="plugin_pyodel", f="sandglass.load",
+                   args=["sandglass", 1], ajax=True)
+    desk=LOAD(c="plugin_pyodel", f="desk.load", ajax=True)
+    return dict(gradebook=gradebook, admission=admission,
+                sandglass=sandglass, desk=desk)
+
